18e6c60912bbb82f20ed8c0fc2007accdc480d85
Jonathan Chiang <chiang810@gmail.com>
2019-04-23 17:31:49 -0700

    Create dash.py

---
+++ dash.py
@@ -0,0 +1,151 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import base64
+import os
+from urllib.parse import quote as urlquote
+from flask import Flask, send_from_directory
+import datetime
+from dash.dependencies import Input, Output, State
+
+
+
+print(dcc.__version__) # 0.6.0 or above is required
+
+external_stylesheets = ['/Users/jonc101/Documents/Biomedical_Data_Science/deep_neuro.css']
+
+server = Flask(__name__)
+app = dash.Dash(server=server,external_stylesheets=external_stylesheets)
+
+
+# Since we're adding callbacks to elements that don't exist in the app.layout,
+# Dash will raise an exception to warn us that we might be
+# doing something wrong.
+# In this case, we're adding the elements through a callback, so we can ignore
+# the exception.
+app.config.suppress_callback_exceptions = True
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
+index_page = html.Div([
+    html.Img(src=app.get_asset_url("deep_neuro.png")),
+    html.Br(),
+    html.Img(src=app.get_asset_url("deep_neuro.png")),
+    html.Br()
+    dcc.Link('Go to Page 1', href='/page-1'),
+    html.Br(),
+    html.Img(src=app.get_asset_url("deep_neuro.png")),
+    html.Br()
+    dcc.Link('Go to Page 2', href='/page-2'),
+])
+
+page_1_layout = html.Div([
+    dcc.Upload(
+        id='upload-image',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=True
+    ),
+    html.Div(id='output-image-upload'),
+])
+
+
+def parse_contents(contents, filename, date):
+    return html.Div([
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
+
+        # HTML images accept base64 encoded strings in the same format
+        # that is supplied by the upload
+        html.Img(src=contents),
+        html.Hr(),
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
+    ])
+
+
+@app.callback(Output('output-image-upload', 'children'),
+              [Input('upload-image', 'contents')],
+              [State('upload-image', 'filename'),
+               State('upload-image', 'last_modified')])
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
+
+
+
+
+@app.callback(dash.dependencies.Output('page-1-content', 'children'),
+              [dash.dependencies.Input('page-1-dropdown', 'value')])
+def page_1_dropdown(value):
+    return 'You have selected "{}"'.format(value)
+
+
+
+page_2_layout = html.Div([
+    html.H1('Page 2'),
+    dcc.RadioItems(
+        id='page-2-radios',
+        options=[{'label': i, 'value': i} for i in ['Orange', 'Blue', 'Red']],
+        value='Orange'
+    ),
+    html.Div(id='page-2-content'),
+    html.Br(),
+    dcc.Link('Go to Page 1', href='/page-1'),
+    html.Br(),
+    dcc.Link('Go back to home', href='/')
+])
+
+
+'''
+
+KEEP
+
+
+
+'''
+
+@app.callback(dash.dependencies.Output('page-2-content', 'children'),
+              [dash.dependencies.Input('page-2-radios', 'value')])
+def page_2_radios(value):
+    return 'You have selected "{}"'.format(value)
+
+
+# Update the index
+@app.callback(dash.dependencies.Output('page-content', 'children'),
+              [dash.dependencies.Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/page-1':
+        return page_1_layout
+    elif pathname == '/page-2':
+        return page_2_layout
+    else:
+        return index_page
+    # You could also return a 404 "URL not found" page here
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True, threaded=True, port = 8051)
